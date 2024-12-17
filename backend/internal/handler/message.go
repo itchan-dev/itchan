1bd86f47a8f71b3e7d5c6d3daa172dcce828f513
unknown <itchandev@gmail.com>
2024-12-17 15:42:12 +0300

    add message handler tests

--- backend/internal/handler/message.go
+++ backend/internal/handler/message.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,51 +9,46 @@ import (
 	"github.com/itchan-dev/itchan/shared/domain"
 )
 
-func (h handler) CreateMessage(w http.ResponseWriter, r *http.Request) {
-	type bodyJson struct {
-		Title       string              `validate:"required" json:"title"`
-		Text        string              `validate:"required" json:"text"`
-		Attachments []domain.Attachment `validate:"required" json:"attachments"`
-	}
-	var body bodyJson
-	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Bad request", http.StatusBadRequest)
-		return
-	}
-	accessCookie, err := r.Cookie("accessToken")
-	if err != nil {
-		http.Error(w, "can't get accessToken cookie", http.StatusUnauthorized)
-		return
-	}
-	jwtClaims, err := h.jwt.DecodeToken(accessCookie.Value)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
-		return
-	}
-	uid, ok := jwtClaims["uid"].(int64)
-	if !ok {
-		http.Error(w, "Can't find uid in jwtClaims", http.StatusInternalServerError)
-		return
-	}
+func (h *handler) CreateMessage(w http.ResponseWriter, r *http.Request) {
+	board := mux.Vars(r)["board"]
 	threadIdStr := mux.Vars(r)["thread"]
 	threadId, err := strconv.Atoi(threadIdStr)
 	if err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
-	_, err = h.message.Create(mux.Vars(r)["board"], &domain.User{Id: uid}, body.Text, body.Attachments, int64(threadId))
+
+	type bodyJson struct {
+		Text        string              `validate:"required" json:"text"`
+		Attachments []domain.Attachment `validate:"required" json:"attachments"`
+	}
+	var body bodyJson
+	if err := loadAndValidateRequestBody(r, &body); err != nil {
+		writeErrorAndStatusCode(w, err)
+		return
+	}
+	uidCtx := r.Context().Value("uid")
+	if uidCtx == nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	uid, ok := uidCtx.(int64)
+	if !ok {
+		log.Printf("Cant parse uid %v", uidCtx)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = h.message.Create(board, &domain.User{Id: uid}, body.Text, body.Attachments, int64(threadId))
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Bad request", http.StatusInternalServerError)
+		writeErrorAndStatusCode(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Created"))
 }
 
-func (h handler) GetMessage(w http.ResponseWriter, r *http.Request) {
+func (h *handler) GetMessage(w http.ResponseWriter, r *http.Request) {
 	// board := mux.Vars(r)["board"]
 	msgIdStr := mux.Vars(r)["message"]
 	msgId, err := strconv.Atoi(msgIdStr)
@@ -65,15 +59,14 @@ func (h handler) GetMessage(w http.ResponseWriter, r *http.Request) {
 
 	msg, err := h.message.Get(int64(msgId))
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Bad request", http.StatusBadRequest)
+		writeErrorAndStatusCode(w, err)
 		return
 	}
 
 	writeJSON(w, msg)
 }
 
-func (h handler) DeleteMessage(w http.ResponseWriter, r *http.Request) {
+func (h *handler) DeleteMessage(w http.ResponseWriter, r *http.Request) {
 	board := mux.Vars(r)["board"]
 	msgIdStr := mux.Vars(r)["message"]
 	msgId, err := strconv.Atoi(msgIdStr)
@@ -83,11 +76,9 @@ func (h handler) DeleteMessage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := h.message.Delete(board, int64(msgId)); err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Bad request", http.StatusBadRequest)
+		writeErrorAndStatusCode(w, err)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("deleted"))
 }
